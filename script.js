03d7548584851dc6d7b8e43a1583359680fd4872
Akin Holo <holoakinseloyin@gmail.com>
2025-04-05 13:42:10 +0100

    Moving to next part

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let countEl = document.getElementById("countEl")
+let previousEntries = document.getElementById("previousEntry")
 
 let count = 0
 
+
 function increment() {
    count += 1
-   countEl.innerText = count
+   countEl.textContent = count
 }
 increment()
 
+// save click
 function save() {
+   let countStr = count + " - "
    console.log(count)
-}
+   previousEntries.textContent += countStr
+   countEl.textContent = 0
+   count = 0
+}  
 save()
+
+let name = "Akin"
+let greeting = "Hi, my name is "
+
+let myGreeting = greeting + name 
+
+console.log(myGreeting)
