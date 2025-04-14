c3d8d977341ae9bfca346b30c09dd6c8a794acf8
Akin Holo <holoakinseloyin@gmail.com>
2025-04-14 13:30:44 +0100

    Update

--- scripts/chromeextension.js
+++ scripts/chromeextension.js
@@ -1,11 +1,32 @@
-let myLeads = [];
+ let myLeads = [];
 
 let inputField = document.getElementById("inputField");
 const saveButton = document.getElementById("saveBtn");
 
+const listOrder = document.getElementById("listOrder");
+
+// passing the value of the input field to the myLeads array
 saveButton.addEventListener("click", () => {
-   myLeads.push(inputField.value); // passing the value of the input field to the myLeads array
-   inputField.value = "";
-   console.log("value pushed to myLeads array: ", myLeads);
-   
+   myLeads.push(inputField.value); 
+   renderLeads();
+   clearInput();
 });
+
+// Log out the items in the myLeads array using a for loop 
+function renderLeads() {
+let listItems = "";
+
+for (let i = 0; i < myLeads.length; i++) {
+
+   listItems += `<li>
+                     <a href="${myLeads[i]}" target="_blank">
+                        ${myLeads[i]}
+                     </a>
+                 </li>`; 
+   };
+   listOrder.innerHTML = listItems;
+}
+
+function clearInput() {
+   inputField.value = "";
+}
